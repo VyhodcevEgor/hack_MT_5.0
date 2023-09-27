5704b18afecff548937383a1750802083200b679
HAXF13D <vovik0312@gmail.com>
2023-09-28 02:45:48 +0300

    Добавлена функция автоматического заполнения базы данных

--- Database/database_filling.py
+++ Database/database_filling.py
@@ -1,6 +1,18 @@
 from Database.settings import user, password, host, db_name, yandex_api_key
+from Database.database_requests import insert_bank_info, insert_availabilities
+from random import sample, randint, choice
 from pprint import pprint
+from sqlalchemy import (
+    create_engine,
+    MetaData,
+    Table,
+)
+from sqlalchemy.ext.declarative import declarative_base
 import requests
+
+engine = create_engine(
+    f"mysql+pymysql://{user}:{password}@{host}/{db_name}?charset=utf8mb4"
+)
 
 
 def calculate_spn(radius_meters):
@@ -27,8 +39,8 @@ def map_search_yandex(lat, lng, query, radius, skip=0, types=0):
         "text": query,
         "lang": "ru_RU",
         "ll": f"{lng},{lat}",
-        "spn": f"{spn[0]},{spn[1]}",
-        "rspn": "1",
+        #"spn": f"{spn[0]},{spn[1]}",
+        #"rspn": "1",
         "results": "50",
         "skip": f"{skip}"
     }
@@ -43,33 +55,102 @@ def map_search_yandex(lat, lng, query, radius, skip=0, types=0):
 
 
 if __name__ == '__main__':
-    lat = 55.7522200
-    lng = 37.6155600
+    # Москва
+    # lat = 55.7522200
+    # lng = 37.6155600
+    # Ставрополь
+    lat = 45.0428
+    lng = 41.9734
+    services = [
+        "Открытие счета", "Получение кредита", "Обмен валюты",
+        "Оформление ипотеки", "Кредиит для бизнеса", "Кредитная карта", "Дебетовая карта"
+    ]
+    load_type = ["Полная", "Средняя", "Малая"]
     for i in range(100):
-        data = map_search_yandex(lat, lng, "ВТБ", 400000, skip=i * 50)
+        data = map_search_yandex(lat, lng, "ВТБ", 4000000, skip=i * 50)
         data = data.get('features')
-        for bank in data:
-            #if bank['properties']['CompanyMetaData']['Categories'][0]['class'] == 'banks':
-            print('*' * 40)
-            coordinates = bank.get('geometry').get('coordinates')
-            company_metadata = bank.get('properties').get('CompanyMetaData')
-            category = company_metadata.get('Categories')[-1].get('class')
-            hours = company_metadata.get('Hours')
-            phones = company_metadata.get('Phones')
-            address = company_metadata.get('address')
-            name = company_metadata.get('name')
-            pprint(coordinates)
-            print(category)
-            #pprint(company_metadata['Categories'][0]['class'])
-            #pprint(bank['properties']['CompanyMetaData']['Hours']['Availabilities'])
-            pprint(hours)
-            #pprint(bank['properties']['CompanyMetaData']['Phones'])
-            pprint(phones)
-            pprint(address)
-            pprint(name)
-            #pprint(bank)
-            print('*' * 40)
-        if len(data) != 50:
+        if len(data) == 0:
             break
+        for bank in data:
+            if bank['properties']['CompanyMetaData']['Categories'][0]['class'] == 'banks':
+                coordinates = bank.get('geometry').get('coordinates')
+                company_metadata = bank.get('properties').get('CompanyMetaData')
+                category = company_metadata.get('Categories')[-1].get('class')
+                hours = company_metadata.get('Hours')
+                phones = company_metadata.get('Phones')
+                address = company_metadata.get('address')
+                name = company_metadata.get('name')
+                text_hours = hours['text']
+                bank_id = insert_bank_info(
+                    bank_name=name,
+                    work_hours=text_hours,
+                    address=address,
+                    services=", ".join(sample(services, randint(1, len(services)))),
+                    latitude=coordinates[1],
+                    longitude=coordinates[0],
+                    load_type=choice(load_type),
+                )
 
+                for day in hours['Availabilities']:
+                    week = {
+                        0: "Дни недели",
+                        1: "Выходные",
+                        2: "Каждый день",
 
+                        3: "Понедельник",
+                        4: "Вторник",
+                        5: "Среда",
+                        6: "Четверг",
+                        7: "Пятница",
+                        8: "Суббота",
+                        9: "Воскресенье",
+
+                    }
+                    day_keys = ['Weekdays', 'Weekend', 'Everyday', 'Monday',
+                                'Tuesday', 'Wednesday', 'Thursday',
+                                'Friday', 'Saturday', 'Sunday'
+                                ]
+                    is_working = []
+                    for day_type in day_keys:
+                        if day.get(day_type) is not None:
+                            is_working.append(True)
+                        else:
+                            is_working.append(False)
+
+                    working_time = "24ч"
+                    if day.get('Intervals') is not None:
+                        working_time = day.get('Intervals')[0]
+
+                    for idx, status in enumerate(is_working):
+                        if status:
+                            if idx == 0:
+                                for j in range(3, 8):
+                                    if working_time != "24ч":
+                                        insert_availabilities(week[j], working_time['from'], working_time['to'], bank_id)
+                                    else:
+                                        insert_availabilities(week[j], "00:00", "00:00", bank_id)
+                                break
+                            elif idx == 1:
+                                for j in range(8, 10):
+                                    if working_time != "24ч":
+                                        insert_availabilities(week[j], working_time['from'], working_time['to'],
+                                                              bank_id)
+                                    else:
+                                        insert_availabilities(week[j], "00:00", "00:00", bank_id)
+                                break
+                            elif idx == 2:
+                                for j in range(3, 10):
+                                    if working_time != "24ч":
+                                        insert_availabilities(week[j], working_time['from'], working_time['to'],
+                                                              bank_id)
+                                    else:
+                                        insert_availabilities(week[j], "00:00", "00:00", bank_id)
+                                break
+                            else:
+                                if working_time != "24ч":
+                                    insert_availabilities(week[idx], working_time['from'], working_time['to'], bank_id)
+                                else:
+                                    insert_availabilities(week[idx], "00:00", "00:00", bank_id)
+                        # print(idx, status)
+            if len(data) != 50:
+                break
--- Database/tables.py
+++ Database/tables.py
@@ -12,7 +12,6 @@ from sqlalchemy import (
     DECIMAL)
 from Database.settings import user, password, host, db_name
 
-
 engine = create_engine(
     f"mysql+pymysql://{user}:{password}@{host}/{db_name}?charset=utf8mb4"
 )
@@ -23,11 +22,22 @@ banks_table = Table('banks_table', metadata,
                            autoincrement=True),
                     Column('bank_name', VARCHAR(70),
                            nullable=False),  # was unique=True
-                    Column('type', VARCHAR(50)),  # Тип отделение или банкомат
+                    Column('work_hours', VARCHAR(60)),
+                    Column('address', VARCHAR(256)),
                     Column('services', VARCHAR(256)),
-                    Column('work_hours', VARCHAR(60)),  # was 25
                     Column('latitude', DECIMAL(6, 4)),
                     Column('longitude', DECIMAL(7, 4)),
-                    Column('load_type', VARCHAR(15)),)
+                    Column('load_type', VARCHAR(15)),
+                    Column('phone', VARCHAR(20)),
+                    )
+availabilities_table = Table('availabilities_table', metadata,
+                             Column('id', INTEGER(), primary_key=True,
+                                    autoincrement=True),
+                             Column('day_of_week', VARCHAR(25)),
+                             Column('time_from', TIME()),
+                             Column('time_to', TIME()),
+                             Column('bank_id', INTEGER(),
+                                    ForeignKey('banks_table.id')),
+                             )
 
 metadata.create_all(engine)
